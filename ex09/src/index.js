2f42fbff699b1d7591ffa467e7aeb4b9c3d1baf1
zivojinminic <zivojinminic05@gmail.com>
2021-09-15 20:01:05 +0200

    subtract of two numbers

---
+++ ex09/src/index.js
@@ -0,0 +1,14 @@
+function main(numOne, numTwo) {
+    //Only change code below this line
+    var sum;
+    var numOne;
+    var numTwo;
+    //Only change code above this line
+    return difference = numOne - numTwo;
+}
+
+console.log(main(15, 8)); //change this line
+console.log(main(25, 8));
+console.log(main(425, 120));
+
+module.exports = main;
