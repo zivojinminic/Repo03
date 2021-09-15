fdbaf8ffca77faa17175a8b0150e8d6dd7edb524
zivojinminic <zivojinminic05@gmail.com>
2021-09-15 16:52:13 +0200

    comments in Javascript

---
+++ ex02/src/index.js
@@ -0,0 +1,17 @@
+function main() {
+    //Only change code below this line
+
+    // This is a single line comment
+
+    /*This
+    is a
+    multi-line
+    comment
+    */
+   
+    //Only change code above this line
+}
+
+console.log(main());
+
+module.exports = main;
