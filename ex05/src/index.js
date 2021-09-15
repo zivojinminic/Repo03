424fc2a7242a32e970c8e82b01e7c1d1b4df3e3a
zivojinminic <zivojinminic05@gmail.com>
2021-09-15 19:27:22 +0200

    Initializing variable

---
+++ ex05/src/index.js
@@ -0,0 +1,17 @@
+function main() {
+    //Only change code below this line
+    //Variable declarations
+
+    //Variable assignments
+
+
+    //Only change code above this line
+    return {
+        studyCapVar,
+        properCamelCase,
+        titleCaseOver
+    }
+}
+
+console.log(main());
+module.exports = main;
