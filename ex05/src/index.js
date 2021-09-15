160b8bc769855bfa4a347174a0232677f90a7ffa
zivojinminic <zivojinminic05@gmail.com>
2021-09-15 19:30:43 +0200

    Initializing variable

--- ex05/src/index.js
+++ ex05/src/index.js
@@ -1,16 +1,8 @@
 function main() {
     //Only change code below this line
-    //Variable declarations
-
-    //Variable assignments
-
-
+    var x = 8;
     //Only change code above this line
-    return {
-        studyCapVar,
-        properCamelCase,
-        titleCaseOver
-    }
+    return x;
 }
 
 console.log(main());
