1e9732703b0c06a3053587874f999ab8ff712f00
zivojinminic <zivojinminic05@gmail.com>
2021-09-15 19:57:05 +0200

    The sum of two numbers

---
+++ ex08/src/index.js
@@ -0,0 +1,14 @@
+function main(numOne, numTwo) {
+    //Only change code below this line
+    var sum;
+    var numOne;
+    var numTwo;
+    //Only change code above this line
+    return sum = numOne + numTwo;
+}
+
+console.log(main(15, 8)); //change this line
+console.log(main(5, 28));
+console.log(main(150, 425));
+
+module.exports = main;
