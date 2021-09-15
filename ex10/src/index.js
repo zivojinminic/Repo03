e2acbd5b3cd925d7d65cff0b03579b4bd3d548e3
zivojinminic <zivojinminic05@gmail.com>
2021-09-15 20:06:16 +0200

    Multiplying two numbers

---
+++ ex10/src/index.js
@@ -0,0 +1,14 @@
+function main(numOne, numTwo) {
+    //Only change code below this line
+    var product;
+    var numOne;
+    var numTwo;
+    //Only change code above this line
+    return product = numOne * numTwo;
+}
+
+console.log(main(15, 3)); //change this line
+console.log(main(25, 8));
+console.log(main(425, 10));
+
+module.exports = main;
